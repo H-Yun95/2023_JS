d76f1436973db6a32782f9cc99590ad3d9d02666
H-Yun95 <shxmqnr951@gmail.com>
2023-02-09 17:41:18 +0900

    230209_01

--- 01/app_1.js
+++ 01/app_1.js
@@ -1,1 +1,11 @@
-alert("hi");
+let fn = 5;
+const sn = 2;
+let myNane = 'yoon';
+
+console.log(fn+sn);
+console.log(fn*sn);
+console.log(fn/sn);
+console.log(myNane);
+
+myNane = 'hayoon';
+console.log(myNane);
