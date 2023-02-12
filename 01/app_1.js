72b678bb5b68d9585e013e56b8937ecb17f0128e
H-Yun95 <shxmqnr951@gmail.com>
2023-02-12 23:17:06 +0900

    230212_01

--- 01/app_1.js
+++ 01/app_1.js
@@ -1,11 +1,16 @@
-let fn = 5;
-const sn = 2;
-let myNane = 'yoon';
+// Objects == {}
+const player = {
+    name : 'yoon',
+    points : 10,
+    fat : true
+};
+// 마치 Python의 딕셔너리
+console.log(player);
+console.log(player.name);  // 출력방법 1
+console.log(player['points']); // 출력방법 2
 
-console.log(fn+sn);
-console.log(fn*sn);
-console.log(fn/sn);
-console.log(myNane);
-
-myNane = 'hayoon';
-console.log(myNane);
+player.fat = false; 
+// const는 전체 값의 변경이 불가능하지, Object 내의 개별 요소는 변경 가능
+player.lastname = 'beef';
+// 단순 선언만으로도 property 추가 가능
+console.log(player);
