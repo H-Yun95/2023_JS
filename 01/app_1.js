f0490c3583e522011c681d83939db22bc425d412
H-Yun95 <shxmqnr951@gmail.com>
2023-02-12 23:57:20 +0900

    230212_02

--- 01/app_1.js
+++ 01/app_1.js
@@ -1,16 +1,30 @@
-// Objects == {}
-const player = {
-    name : 'yoon',
-    points : 10,
-    fat : true
+// Function
+function sayHello(name, age){
+    console.log('Hello, my nane is ' + name + ". and I'm " + age);
 };
-// 마치 Python의 딕셔너리
-console.log(player);
-console.log(player.name);  // 출력방법 1
-console.log(player['points']); // 출력방법 2
+// Python의 함수와 동일
+sayHello('Choi', 29);
+sayHello('Ha', 15);
+sayHello('Yoon', 65);
 
-player.fat = false; 
-// const는 전체 값의 변경이 불가능하지, Object 내의 개별 요소는 변경 가능
-player.lastname = 'beef';
-// 단순 선언만으로도 property 추가 가능
-console.log(player);
+function Plus(a, b){
+    console.log(a + b);
+};
+
+Plus(3, 4);
+
+function Divide(a, b){
+    console.log(a / b);
+};
+
+Divide(18, 6)
+
+const Player = {
+    name: 'yoon',
+    sayHi: function(){
+        console.log('Hi.')
+    }, // 객체 내부에서도 함수를 정의 가능
+};
+
+console.log(Player.name);
+Player.sayHi();
