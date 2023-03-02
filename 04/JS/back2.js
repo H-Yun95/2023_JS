0a074ecc6a7060f6c9b8a5d3646e76b0bee9184a
H-Yun95 <shxmqnr951@gmail.com>
2023-03-02 21:44:05 +0900

    230302_02

---
+++ 04/JS/back2.js
@@ -0,0 +1,22 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg"
+];
+
+const imageChoise = images[Math.floor(Math.random() * images.length)];
+const changeIMG = document.querySelector(".changeIMG button")
+const BGImage = document.querySelector("body");
+
+function changeBG(e) {
+    e.preventDefault()
+    BGImage.style.backgroundImage = imageChoise;
+    console.log(imageChoise)
+}
+
+// Fail
+changeIMG.addEventListener("submit, ")
